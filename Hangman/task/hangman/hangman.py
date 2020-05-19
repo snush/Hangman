bb6e3e821386a0775ecfc308ab1f0dc04ffc5f06
Sveta Ushakova <53977091+snush@users.noreply.github.com>
2020-05-19 23:52:21 +0300

    Update hangman.py

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,21 +1,20 @@
 import random
 print("H A N G M A N")
 while input('Type "play" to play the game, "exit" to quit: ') == "play":
-    language = ['python', 'java', 'kotlin', 'javascript']
+    languages = ['python', 'java', 'kotlin', 'javascript']
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    answer = []
-    word = random.choice(language)
+    answers = []
+    word = random.choice(languages)
     new_word = "-" * len(word)
     attempts = 8
     while new_word.count("-") and attempts != 0:
-        print()
-        print(new_word)
+        print("\n" + new_word)
         letter = input("Input a letter: ")
         if len(letter) != 1:
             print("You should print a single letter")
         elif alphabet.count(letter) == 0:
             print("It is not an ASCII lowercase letter")
-        elif answer.count(letter):
+        elif answers.count(letter):
             print("You already typed this letter")
         elif word.count(letter) and letter == "a":
             new_word = new_word[:word.find(letter)] + letter + new_word[word.find(letter) + 1:word.rfind(letter)] + letter + new_word[word.rfind(letter) + 1:]
@@ -26,9 +25,8 @@ while input('Type "play" to play the game, "exit" to quit: ') == "play":
         else:
             print("No such letter in the word")
             attempts -= 1
-        answer.append(letter)
+        answers.append(letter)
     if attempts != 0:
-        print("You guessed the word!\nYou survived!")
+        print("You guessed the word!\nYou survived!\n")
     else:
-        print("You are hanged!")
-    print()
+        print("You are hanged!\n")
